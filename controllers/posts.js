e584e3fb9d5fbfcd06d42dccb73bf651d25d6b09
YeeeeRQ <YeeeeRQ@outlook.com>
2023-08-16 22:41:11 +0800

    update Functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,28 +1,44 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessage = await PostMessage.find();
-        console.log(postMessage)
+  try {
+    const postMessage = await PostMessage.find();
+    console.log(postMessage);
 
-        res.status(200).json(postMessage)
-        
-    } catch (error) {
-        res.status(404).json({message:error.message})
-        console.log(error)
-    }
-}
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+    console.log(error);
+  }
+};
 
 export const createPost = async (req, res) => {
-    const post = req.body;
-    const newPost = new PostMessage(post);
-    try {
-        await newPost.save();
-        res.status(201).json(newPost)
-        // https://restapitutorial.com/httpstatuscodes.html
-        
-    } catch (error) {
-        res.status(409).json({message:error.message})
-        
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+    // https://restapitutorial.com/httpstatuscodes.html
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with taht id");
+
+  const updatePost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
     }
-}
+  );
+
+  res.json(updatePost);
+};
