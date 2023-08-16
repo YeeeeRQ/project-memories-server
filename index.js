768f587198946a2ff9117bc83c91dd452813796b
YeeeeRQ <YeeeeRQ@outlook.com>
2023-08-17 00:17:45 +0800

    env

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from 'dotenv'
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config()
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use('/posts', postRoutes)
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://yeeeerq:linuxcast@cluster0.rzyi5pu.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,4 +32,3 @@ mongoose
   .catch((error) => {
     console.log(error.message);
   });
-
